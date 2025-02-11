9803846a4f41f7a81725f1ffd8afa9649b4ab3ea
sonali173 <sonalisinghsoomo@gmail.com>
2025-02-11 16:04:11 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,1 @@
 
-        const slides = document.querySelector(".slides");
-        const slide = document.querySelectorAll(".slide");
-        let index = 0;
-
-        function nextSlide() {
-            index = (index + 1) % slide.length;
-            updateSlide();
-        }
-
-        function prevSlide() {
-            index = (index - 1 + slide.length) % slide.length;
-            updateSlide();
-        }
-
-        function updateSlide() {
-            slides.style.transform = `translateX(-${index * 100}%)`;
-        }
-        setInterval(nextSlide, 3000);
